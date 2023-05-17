7cf7290ee7343d5b3d600ff71b6e74bccfa1a1b7
María José Porras <marijopm27@estudiantec.cr>
2023-05-17 13:51:31 -0600

    Ver Planes de Trabajo

--- Proyecto2/frontend/client/src/pages/plan.jsx
+++ Proyecto2/frontend/client/src/pages/plan.jsx
@@ -1,7 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect} from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export default function PlanList() {
+  const [cards, setCards] = React.useState([])
+  useEffect(() => {
+    axios.get(`http://localhost:4000/profesor/VerPlanesTrabajo`)
+        .then((response) => {
+            console.log(response.data)
+            setCards(response.data);
+        })
+}, []);
+
+const cardElements = cards.map(card => (
+  <div key={card.IDplanTrabajo} className="card-tile">
+      <div className="card-info">
+          <Link to={`/planList/plan/${card.IDplanTrabajo}`}>{card.Nombre}</Link>
+          <p>{card.Abreviacion}<span></span></p>
+          <p>{card.IDcoordinador}<span></span></p>
+      </div>
+  </div>
+))
+
   const [plans] = useState([
     { id: 1, name: 'Plan 1' },
     { id: 2, name: 'Plan 2' },
@@ -10,14 +30,12 @@ export default function PlanList() {
 
   return (
     <div>
-      <h1>Planes de Trabajo</h1>
-      <ul>
-        {plans.map((plan) => (
-          <li key={plan.id}>
-            <Link to={`/planList/plan/${plan.id}`}>{plan.name}</Link>
-          </li>
-        ))}
-      </ul>
+      <div className="card-list-container">
+        <h1>Planes de Trabajo</h1>
+        <div className="card-list">
+            {cardElements}
+        </div>
+      </div>
     </div>
   );
 };
