75a2225bea4daf2f299201cfa9060bfea4f1951b
María José Porras <marijopm27@estudiantec.cr>
2023-05-18 16:07:17 -0600

    Ajustes de formato de plan y actividades

--- Proyecto2/frontend/client/src/pages/actividad.jsx
+++ Proyecto2/frontend/client/src/pages/actividad.jsx
@@ -6,7 +6,7 @@ export default function ActivityList(){
   const [activities, setactivities] = useState([])
     useEffect(() => {
       axios.post(`http://localhost:4000/profesor/VerActividadxPlan`, {
-        IDplanTrabajo:IDplanTrabajo
+        IDplanTrabajo:planId
       }
       , {
         headers: {
@@ -23,16 +23,40 @@ export default function ActivityList(){
   const filteredActivities = activities.filter((activity) => activity.IDplanTrabap === Number(planId));
 
   return (
+
     <div>
-      <h1>Actividades del Plan {IDplanTrabajo}</h1>
-      <ul>
-        {activities.map((activity) => (
-          <li key={activity.IDactividad}>
-            <Link to={`/planList/plan/${IDplanTrabajo}/activity/${activity.IDactividad}`}>{activity.Nombre}</Link>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <section>
+      <div className="container">
+        <h1>Actividades del Plan {planId}</h1>
+        <div className="cards">
+          {activities.map((activity) => (
+            <div key={activity.IDplanTrabajo} className="card">
+              <Link to={`/planList/plan/${planId}/activity/${activity.IDactividad}`}>{activity.IDactividad}</Link>
+              
+              <h3> Semana:</h3>
+              <p>{activity.Semana}</p>
+              <h3>Fecha: </h3>
+              <h4>{activity.Fecha}</h4>
+              
+              {/* <button className="btn" onClick={navegar(plan.IDplanTrabajo)}>Explore</button> */}
+              
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  </div> 
+
+    // <div>
+    //   <h1>Actividades del Plan {planId}</h1>
+    //   <ul>
+    //     {activities.map((activity) => (
+    //       <li key={activity.IDactividad}>
+    //         <Link to={`/planList/plan/${planId}/activity/${activity.IDactividad}`}>{activity.IDactividad}</Link>
+    //       </li>
+    //     ))}
+    //   </ul>
+    // </div>
   );
 };
 
