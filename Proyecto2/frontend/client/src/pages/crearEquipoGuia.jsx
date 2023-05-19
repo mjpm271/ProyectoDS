c2b037bec02131b04e2976329f07bd6820149163
batomas13 <tcoto2003@gmail.com>
2023-05-18 19:38:46 -0600

    pagina para crearequipoguia

---
+++ Proyecto2/frontend/client/src/pages/crearEquipoGuia.jsx
@@ -0,0 +1,48 @@
+import axios from 'axios';
+import React, { useState  } from 'react';
+import { Button,  Form, Dropdown, DropdownItem, DropdownMenu, Label} from 'semantic-ui-react'
+import Navbar from "../components/Navbar"
+import Footer from '../components/Footer';
+
+export default function CrearEquipoGuia() {
+    const [Nombre, setNombre] = useState();
+    const postData = () => {
+        
+        axios.post('http://localhost:4000/asistente/CrearEquipoGuia', {
+            Nombre:Nombre,
+          }
+          , {
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          }
+          )
+            .then(response => {
+              console.log(response.data);
+            }).catch(error => {
+                console.log(error)
+            });
+
+        // fetch('http://localhost:4000/ejemplo/asistente/AgregarProfesor',{
+        //     method : 'post',
+        //     mode: 'cors',
+        //     body: formData,
+        //     headers: { "Content-Type": "multipart/form-data" }
+        // })
+
+        //axios.post(`http://localhost:4000/ejemplo/asistente/AgregarProfesor`,{ID,Nombre,Correo,Contra,Habilitado,Coordinador,Sede,IDtipo})
+    }
+    return (
+        <div>
+            <Navbar />
+            <Form className="create-form">
+                <Form.Field>
+                    <label>Nombre </label>
+                    <input placeholder='Nombre' onChange={(e) => setNombre(e.target.value)}/>
+                </Form.Field>
+                <Button onClick={postData} type='submit'>Submit</Button>
+            </Form>
+            <Footer/>
+        </div>
+    )
+}
