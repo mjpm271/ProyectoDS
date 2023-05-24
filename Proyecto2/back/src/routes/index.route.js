16f7b91799ed68d8afd73d8828e98a16ce805071
María José Porras <marijopm27@estudiantec.cr>
2023-05-23 20:35:43 -0600

    Cambio Contrase;a

--- Proyecto2/back/src/routes/index.route.js
+++ Proyecto2/back/src/routes/index.route.js
@@ -1,8 +1,8 @@
 import { Router } from "express"
-import {login} from '../controllers/index.controller'
+import {login, CambiarContraseña} from '../controllers/index.controller'
 
 const router = Router()
 
 router.post('/login', login)
-
+router.put('/CambiarContraseña',CambiarContraseña)
 export default router
