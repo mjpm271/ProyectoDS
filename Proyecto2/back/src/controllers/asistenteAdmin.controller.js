b3f69ab9255f51d11e7a4898d945f784d404544c
Tomas <tcoto2003@gmail.com>
2023-05-28 19:27:45 -0600

    se arregla modificar profesor

--- Proyecto2/back/src/controllers/asistenteAdmin.controller.js
+++ Proyecto2/back/src/controllers/asistenteAdmin.controller.js
@@ -275,7 +275,7 @@ export const BuscarProfesor = async (req, res) => {
 export const ModificarProfesor = async (req, res) => {
     res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // --> posiblemente haya que cambiar el lugar de acceso dependiendo de la pag que viene
     res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept');    
-    const { IDpersona, NombreCompleto, Correo, Contra,Foto,Habilitado, Coordinador, Telefono, TelefonoOficina,Sede, IDtipo } = req.body
+    const { Carnet, NombreCompleto, Correo, Contra,Foto,Habilitado, Coordinador, Telefono, TelefonoOficina,Sede, IDtipo } = req.body
     console.log('valores:', req.body)
     /*if (!IDpersona || !NombreCompleto || !Correo || !Contra || !Habilitado || !Coordinador || !Sede || !IDtipo) {
         console.log('here')
@@ -284,21 +284,23 @@ export const ModificarProfesor = async (req, res) => {
     try {
         const pool = await getConnection();
         //console.log('whatever')
-        const result = await pool
+       const result = await pool
             .request()
-            .input('ID', sql.Int, IDpersona)
+            .input('Carnet', sql.VarChar(64), Carnet)
             .input('NombreCompleto', sql.VarChar(100), NombreCompleto)
             .input('Correo', sql.VarChar(100), Correo)
             .input('Contra', sql.VarChar(64), Contra)
-            .input('Foto',sql.VarChar(100),Foto)
+            .input('Foto',sql.VarChar(sql.MAX),Foto)
             .input('Habilitado', sql.Bit, Habilitado)
             .input('Coordinador', sql.Bit, Coordinador)
             .input('Telefono',sql.VarChar(64),Telefono)
             .input('TelefonoOficina',sql.VarChar(64),TelefonoOficina)
             .input('Sede', sql.Int, Sede)
             .input('IDtipo', sql.Int, IDtipo) //Preguntar si sería bueno setear desde el inicio a 1 como profesor
+            .output('Exito',sql.Int)
             .execute('UpdatePersona')
         console.log(result)
+
     } catch (err) {
         res.sendStatus(500, err.message)
     }
