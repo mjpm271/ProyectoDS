c702b7fcc7d51bfec4aecf3ecbb65f40926fc90f
María José Porras <marijopm27@estudiantec.cr>
2023-05-29 15:31:05 -0600

    Sin icons

--- Proyecto2/frontend/client/src/components/Navbar.js
+++ Proyecto2/frontend/client/src/components/Navbar.js
@@ -1,5 +1,5 @@
 import { useRef } from "react";
-import { FaBars, FaTimes } from "react-icons/fa";
+// import { FaBars, FaTimes } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 import "../index.css";
 
