80e24ff5ecb59daf69dc64b5033f517f1d18aec0
María José Porras <marijopm27@estudiantec.cr>
2023-06-07 20:02:53 -0600

    Faltaba 1

--- Proyecto2/frontend/client/src/pages/actividad.jsx
+++ Proyecto2/frontend/client/src/pages/actividad.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link, useParams, useLocation } from 'react-router-dom';
 import { Grid, Segment , List, Button} from 'semantic-ui-react';
 import Comments from "../comentarios/Comments";
-import Navbar from '../components/Navbar'
+// import Navbar from '../components/Navbar'
 import axios from 'axios';
 import NavBar from '../components/NavBar2';
 
