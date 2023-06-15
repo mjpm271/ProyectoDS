3afa6fcb3e0eb17d74cb9f9e93bcdd34aee263a2
Adrian Ramirez Mattey <aramirezmattey@gmail.com>
2023-06-14 23:59:48 -0600

    Update chat.controller.js

--- Proyecto2/back/src/controllers/chat.controller.js
+++ Proyecto2/back/src/controllers/chat.controller.js
@@ -1,5 +1,62 @@
 import { getConnection, sql } from "../database/connection"
 
+export const CrearSalaChat = async (req, res) => {
+    const {NombreChat} = req.body
+    if (!NombreChat) {
+        return res.status(400).json({ msg: 'Bad Request. Please fill all fields' })
+    }
+
+    try {
+        const pool = await getConnection()
+        const result = await pool
+            .request()
+            .input('Nombre', sql.VarChar(255), NombreChat)
+            .output('resultado', sql.Int)
+            .execute('CreateChats')
+        const outputValue = result.output
+
+        if (outputValue === 0) {
+            return res.sendStatus(500, 'Could not create new chatroom')
+        }
+        res.json(outputValue)
+    } catch (err) {
+        res.sendStatus(500, err.message)
+    }
+}
+
+export const LeerSalasChat = async (req, res) => {
+    try {
+        const pool = await getConnection()
+        const result = await pool
+            .request()
+            .execute('ReadChats')
+        console.log('Chatrooms:', result)
+        res.json(result.recordset)
+    } catch (err) {
+        return res.sendStatus(500, err.message)
+    }
+}
+
+export const ActualizarSalaChat = async (req, res) => {
+    const {IDSalaChat, NombreChat} = req.body
+    console.log('Chatroom:', IDSalaChat, 'New name:', NombreChat)
+    if (!IDSalaChat || !NombreChat) {
+        return res.status(400).json({ msg: 'Bad request. Please fill all fields' })
+    }
+
+    try {
+        const pool = await getConnection()
+        const result = await pool
+            .request()
+            .input('IDChat', sql.Int, IDSalaChat)
+            .input('nuevoNombre', sql.VarChar(255), NombreChat)
+            .execute('UpdateChat')
+        
+    } catch (err) {
+        
+    }
+}
+
 export const EnviarMensaje = async (req, res) => {
     const { IDUsuario, Mensaje, Fecha, IDSalaChat } = req.body
     console.log('User info:', IDUsuario)
@@ -20,6 +77,11 @@ export const EnviarMensaje = async (req, res) => {
             .execute('CreateMensaje')
         const outputValue = result.output
 
-        
+        res.json(outputValue)
+
+    } catch (err) {
+        res.sendStatus(500, err.message)
     }
+
+
 }
